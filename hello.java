741643a019d4edea1dc43d044cbf11e26122c204
Joshua Stambaugh <joshstambaugh@arizona.edu>
2024-04-15 17:15:39 -0700

    translator push

---
+++ hello.java
@@ -0,0 +1,14 @@
+public class hello {
+	public static void main(String[] args) {
+		int numberOne = 7;
+		int numberTwo = 13;
+		System.out.println(numberOne);
+		System.out.println(numberTwo);
+		boolean boolOne = true;
+		boolean boolTwo = false;
+		System.out.println(boolOne);
+		System.out.println(boolTwo);
+		int numberThree = 3321;
+		System.out.println(numberThree);
+	}
+}
--- translate.py
+++ translate.py
@@ -1,5 +1,4 @@
 def openFile(filename):
-    
     try:
         with open(filename, 'r') as file:
             file_contents = file.read()
@@ -17,9 +16,9 @@ def removeComments(contents):
     inComment = False
 
     for item in contents:
-        if item == "%" and not inComment:
+        if (item == "%" or item[0] == "%") and not inComment:
             inComment = True
-        elif item == "%" and inComment:
+        elif (item == "%" or item[-1] == "%") and inComment:
             inComment = False
         elif not inComment:
             result.append(item)
@@ -27,20 +26,45 @@ def removeComments(contents):
 
 # turns into lines of code
 def createLines(input):
-    acc = 0
     result = []
     cur = []
+    in_string = False
+    string_token = ""
+
     for item in input:
-        
-        if item[len(item)-1] == ".":
-            cur.append(item[0:-1])
-            acc += 1
-            result.append(cur)
-            cur = []
+        if item[0] == '"' and not in_string:
+            in_string = True
+
+            string_token = item  # Remove the opening quotation mark
+
+            if item[-2] == '"':
+                in_string = False
+                cur.append(string_token[:-1])
+                string_token = ""
+
+
+            
+        elif in_string:
+            if item[-2] == '"':
+                in_string = False
+                string_token += item[:-1]  # Remove the closing quotation mark
+                cur.append(string_token)
+                string_token = ""
+            else:
+                string_token += " " + item
         else:
-            cur.append(item)
-    #print("There should be " + str(acc) + " 'lines'")
+            if item.endswith("."):
+                cur.append(item[:-1])  # Remove the period
+                result.append(cur)
+                cur = []
+            else:
+                cur.append(item)
+
+    if cur:
+        result.append(cur)
+
     return result
+
 
 # creates java class with main
 def startFile(filename):
@@ -49,12 +73,11 @@ def startFile(filename):
         file = open(filename_ext, 'w')
 
         class_line = "public class " + filename + " {\n"
-        main_line = "\tpublic static void main(String[] args) {"
+        main_line = "\tpublic static void main(String[] args) {\n"
 
         lines = [class_line, main_line]
 
         file.writelines(lines)
-
 
         return file
     except (IOError, OSError) as e:
@@ -63,156 +86,105 @@ def startFile(filename):
 
 # ends the main and class
 def endFile(file):
-    end_main = "\n\t}"
-    end_class = "\n}"
+    end_main = "\n\t}\n"
+    end_class = "}\n"
     lines = [end_main, end_class]
     file.writelines(lines)
-    
-
+    file.close()
 
 # assigns the line to a line number, and to a line type
 def typeLines(lines):
     line_number = 0
     result = {}
     for line in lines:
-
-
-
         if line[1] == "is":
             result[line_number] = [line, "ASSIGNMENT"]
-
-        # KEVIN: Please change this to how you were thinking -Josh
         elif line[0] == "print":
             result[line_number] = [line, "PRINT"]
-
-
-
-
-
-
-
-
-
-
-
         line_number += 1
     return result
-            
-# just after thinking if we have parenthesis it could pose a challenge but not 
-# too concerned rn just trying to get a very basic idea
-# also does work with somehting like "val is 7 add 3" will probably require lots of ifs
-# if anyone has a better idea please feel free
-def translate(file, dict):
-    write_lines = []
-    for item in dict:
 
-        
-        # each typed line entered
-        dict_line = dict[item]
+def translate(java_file, source_code_dict):
+    print("ENTER TRANSLATE")
+    for line_number, (line, line_type) in source_code_dict.items():
+        if line_type == "ASSIGNMENT":
+            variable = line[0]
+            expression = translate_expression(line[2:])
+            var_type = determine_var_type(expression)
+            java_line = f"\t\t{var_type} {variable} = {expression};\n"
+            print(java_line)
+            java_file.write(java_line)
+        elif line_type == "PRINT":
+            expression = translate_expression(line[1:])
+            java_line = f"\t\tSystem.out.println({expression});\n"
+            print(java_line)
+            java_file.write(java_line)
 
-        # expression type
-        expr = dict_line[1]
+def determine_var_type(expression):
+    print("ENTER DETERMINE VAR TYPE")
+    if expression[0] == '"' and expression[-1] == '"':
+        return "String"
+    elif expression[0].lower() in ["t", "f"]:
+        return "boolean"
+    elif expression[0].isdigit():
+        return "int"
+    else:
+        return "String"
 
+def translate_expression(tokens):
+    print("ENTER TRANSLATE EXPRESSION")
+    if len(tokens) == 1:
+        return translate_value(tokens[0])
+    elif len(tokens) >= 3:
+        operator = tokens[1]
+        left_operand = translate_value(tokens[0])
+        right_operand = translate_expression(tokens[2:])
+        if operator in ["add", "+"]:
+            return f"{left_operand} + {right_operand}"
+        elif operator in ["sub", "-"]:
+            return f"{left_operand} - {right_operand}"
+        elif operator in ["mult", "*"]:
+            return f"{left_operand} * {right_operand}"
+        elif operator in ["div", "/"]:
+            return f"{left_operand} / {right_operand}"
+        elif operator in ["mod", "%"]:
+            return f"{left_operand} % {right_operand}"
+        elif operator in ["AND", "OR"]:
+            if operator == "AND":
+                return str(left_operand)+ " && " +str(right_operand)
+            else:
+                return str(left_operand)+ " || " +str(right_operand)
+    else:
+        raise ValueError(f"Invalid expression: {tokens}")
 
-        if expr == "ASSIGNMENT":
-            i = 0
-
-            # code as list of strings
-            code_line = dict_line[0]
-
-            val = code_line[len(code_line)-1]
-            name = code_line[0]
-
-            #print("VAL '"+val+"'")
-
-
-            try :
-                int(val)
-
-                to_write = "\n\t\tint " + name + " = " + val + ";"
-                write_lines.append(to_write)
-
-            except:
-                pass
-
-            # not sure if this try is necessary, i am tired
-            try :
-                #val_str = str(val)
-                if val[0] == "T" or val[0] == "F":
-                    to_write = ""
-                    if val[0] == "T":
-                        to_write = "\n\t\tboolean " + name + " = true;"
-                    else:
-                        to_write = "\n\t\tboolean " + name + " = false;"
-                    write_lines.append(to_write)
-                    pass
-                elif val[0] == '"':
-                    # this is for strings
-                    pass
-            except:
-                pass
-
-
-
-
-
-
-
-        elif expr == "PRINT":
-            val = dict_line[0][1]
-            to_write = "\n\t\tSystem.out.println("+val+");"
-            write_lines.append(to_write)
-            
-    
-
-    file.writelines(write_lines)
-
-
-
-
-
-
-
+def translate_value(token):
+    print("ENTER TRANSLATE VALUE")
+    if token.startswith("\"") and token.endswith("\""):
+        return token
+    elif token.isdigit():
+        return token
+    elif token in ["T", "F"]:
+        return "true" if token == "T" else "false"
+    else:
+        return token
 
 def main():
     filename = input("Enter the filename (txt extension): ")
-    filename_full = filename+".txt"
+    filename_full = filename + ".txt"
     file_contents = openFile(filename_full)
-    
     contents_list = file_contents.split()
     source_code = removeComments(contents_list)
+    #print("AFTER COMMENTS "+ str(source_code))
 
-    # at this point, source_code is a list of lines, each line is a list of strings indicating each keyword
+
+
     source_code = createLines(source_code)
-    #printLinesDebug(source_code)
 
-    # at this point, source_code_dict assigns each line a line number, and a type
+    print("AFTER CREATE LINES "+ str(source_code))
     source_code_dict = typeLines(source_code)
-    #printDictLinesDebug(source_code_dict)
-
-
-    # builds file
     java_file = startFile(filename)
     translate(java_file, source_code_dict)
     endFile(java_file)
 
-
-
-
-
-# DEBUG FUNCTIONS - DELETE LATER
-def printLinesDebug(lines):
-    print("START OF LINES")
-    for line in lines:
-        print(line)
-    print("END OF LINES\n")
-
-def printDictLinesDebug(lines):
-    print("START OF DICTIONARY")
-    for line in lines:
-        print("LINE #"+str(line) + "==" + str(lines[line]))
-        
-    print("END OF DICTIONARY\n")
-# END DEBUG
-
-main()
+if __name__ == "__main__":
+    main()
