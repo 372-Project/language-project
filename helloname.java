951835419fc4d9c6d50e9a6bf16780e0947d39cb
Joshua Stambaugh <joshstambaugh@arizona.edu>
2024-04-23 16:09:52 -0700

    string thing

---
+++ helloname.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class helloname {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Please enter your name\n");
+		String name = in.nextLine();
+		String begin = "Hello ";
+		String ending = "!! Thanks for using our language!";
+		String output = begin + name + ending;
+		System.out.print(output);
+
+		in.close();
+	}
+}
